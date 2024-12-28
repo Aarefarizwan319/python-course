d8dbe32edceff795a98d2807ae5e8812624decb0
Aarefarizwan319 <Aarefazahra359@gmail.com>
2024-12-28 21:35:33 +0400

    data types

---
+++ lesson 2/data type.py
@@ -0,0 +1,15 @@
+name = "Aarefa "
+age= 15
+weight = 34.4
+is_student= True
+print("Name :" , name)
+print("Data type is " , type(name))
+print("Age:" , age)
+print("Data type is",type(age))
+print("Weight :" , weight)
+print("Data type is " , type(weight))
+print("is_student :" , is_student)
+print("Data type is " , type(is_student))
+print("After type casting")
+weight= int(weight)
+print("weight is", weight)
