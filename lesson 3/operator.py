6de8ed3bd16d72434d8ccee321ad5c9f73edf3cc
Aarefarizwan319 <Aarefazahra359@gmail.com>
2024-12-28 21:52:30 +0400

    operators

---
+++ lesson 3/operator.py
@@ -0,0 +1,13 @@
+num1 = 11
+num2 = 23
+print("addition is",num1+num2)
+print("subtraction is",num1-num2)
+print("multiplication is",num1*num2)
+print("division is",num1/num2)
+print("modulas is",num1%num2)
+print("square is",num1**2)
+print("floor division is",num1//num2)
+print("number 1 less than",num1<num2)
+print("number 2 greator than",num1>num2)
+print("num1 and num2 is equal ",num1==num2)
+print("num1 and num2 is not equal ",num1!=num2)
