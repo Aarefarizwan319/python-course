07a89bb5a2629363d93e0f2f0e0f1a4ec48a622b
Aarefarizwan319 <Aarefazahra359@gmail.com>
2025-01-18 21:29:34 +0400

    function

---
+++ lesson 4/Untitled-1.py
@@ -0,0 +1,12 @@
+def intro(name):
+    print("Hello , Good Morning!I am " , name)
+    
+user_name = input("Enter your name")
+user_hobby = input("Enter your hobby")
+intro(user_name)
+
+def myself(hobby):
+    print("My Hobby is : " , hobby)
+    
+
+myself(user_hobby)
