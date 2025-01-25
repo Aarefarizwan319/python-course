7772fe1a1ca2f45c9707eef2a5cc4cce2280a562
Aarefarizwan319 <Aarefazahra359@gmail.com>
2025-01-25 17:36:31 +0400

    armstrong

---
+++ lesson 4/armstrong.py
@@ -0,0 +1,12 @@
+def is_armstrong_number(num):
+    num_str = str(num)
+    num_digits = len(num_str)
+    sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
+    return sum_of_powers == num
+
+user_input = int(input("Enter a number to check if it is an Armstrong number: "))
+
+if is_armstrong_number(user_input):
+    print(f"{user_input} is an Armstrong number.")
+else:
+    print(f"{user_input} is not an Armstrong number.")
