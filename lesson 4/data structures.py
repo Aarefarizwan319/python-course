5bc274e3fce7cb847769b7bb132eb7c4a2dace40
Aarefarizwan319 <Aarefazahra359@gmail.com>
2025-01-28 21:24:43 +0400

    list

---
+++ lesson 4/data structures.py
@@ -0,0 +1,18 @@
+mylist=['Apple', 'Mango','Cherry','Berry']
+print("Length of list:", len(mylist))
+print("First element:", mylist[0])
+print("Last elemnt:", mylist[-1])
+mylist.remove('Apple')
+print("Updated list:",mylist)
+mylist.pop(2)
+print("Updated list:",mylist)
+mylist.sort()
+print("Sorted list:",mylist)
+mylist.append('cherry')
+print("Updated list:",mylist)
+mylist.reverse()
+print("Reversed list:",mylist)
+
+
+
+
